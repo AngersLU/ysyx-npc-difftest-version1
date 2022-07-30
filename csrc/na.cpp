cf34a73b97e3d4af07d4ed47aaf2374566b2353c
AngreLU <87403367+AngreLU@users.noreply.github.com>
2022-07-30 22:20:22 +0800

    Add files via upload

--- csrc/na.cpp
+++ csrc/na.cpp
@@ -13,9 +13,13 @@
 #include "svdpi.h"
 #include "Vtop__Dpi.h"
 #include "verilated_dpi.h"
+#include "device.h"
 
+using namespace std;
 #define CONFIG_MBASE 0x80000000
 #define CONFIG_MSIZE 0x8000000
+#define CONFIG_RTC_MMIO 0xa0000048
+#define CONFIG_SERIAL_MMIO 0xa00003f8
 
 typedef uint64_t paddr_t;
 
@@ -28,41 +32,59 @@ void init_difftest(char *ref_so_file, long img_size, int port);
 
 VerilatedContext *contextp = NULL;
 VerilatedVcdC *tfp = NULL;
-static Vname *top;
+Vname *top;
 static vluint64_t main_time = 0;
 static const vluint64_t sim_time = 1000;
 
-using namespace std;
+
 bool is_exit = false;
+bool isebreak = false;
+uint64_t *cpu_gpr = NULL;
 
 struct CPU_state
 {
   uint64_t gpr[32];
   uint64_t pc;
+
+  uint64_t mepc;
+  uint64_t mstatus;
+  uint64_t mcause;
+  uint64_t mtvec;
 }cpuu;
 
 
 // DPI-C
-bool isebreak = false;
 extern "C" void ebreak() {
   isebreak = true;
 }
 
-uint64_t *cpu_gpr = NULL;
 extern "C" void set_gpr_ptr(const svOpenArrayHandle r) {
   cpu_gpr = (uint64_t *)(((VerilatedDpiOpenVar *)r)->datap());
   //cpuu.pc = top->pc;
 }
 
+// extern "C" void mmio_read(paddr_t addr, int len) {
+//   //对mmio的两个外设进行读取
+//   return map_read(addr, len, fetch_mmio_map(addr));
+// }
+
+// extern "C" void mmio_write() {
+//   //对mmio的两个外设进行写入
+//   map_write(addr, len, data, fetch_mmio_mapp(addr));
+// }
+
 
 // DPI-C
 extern "C" void mem_read(long long raddr, long long *rdata) { 
-  if(raddr<0x88000000 && raddr > 0x80000000 ){
-    if( top->dsram_e && !top->dsram_we ){
+  if(raddr<0x88000000 && raddr >= 0x80000000 ){
     // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
-     // pmem_read(      *(uint64_t *)(raddr & ~0x7ull) ;//;
-      *rdata = pmem_read((raddr & ~0x7ull), 8);
-    }
+    // pmem_read(      *(uint64_t *)(raddr & ~0x7ull) ;//;
+   // *rdata = pmem_read((raddr & ~0x7ull), 8) >> ((raddr & 0x7ull) * 8);
+    *rdata = pmem_read((raddr & ~0x7ull), 8);
+  }
+  if (raddr == CONFIG_RTC_MMIO) {
+    // printf("%08lx\n", get_time(0));
+    *rdata = get_time();
   }
 }
 
@@ -70,27 +92,30 @@ extern "C" void mem_write(long long waddr, long long wdata, char wmask) {
   // 总是往地址为`waddr & ~0x7ull`的8字节按写掩码`wmask`写入`wdata`
   // `wmask`中每比特表示`wdata`中1个字节的掩码,
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
-  if(waddr<0x88000000 && waddr > 0x80000000 ){
-    if( top->dsram_e && top->dsram_we ){
-      long long mask = 0;
-      for(int i = 0; i < 8; i++){
-        if((wmask >> i) & 0x01){
-          long long f = 0xff; 
-          f = f << (i * 8);
-          mask |= f;
-        }
+  if(waddr<0x88000000 && waddr >= 0x80000000 ) {
+    long long mask = 0;
+    for(int i = 0; i < 8; i++){
+      if((wmask >> i) & 0x01){
+      long long f = 0xff; 
+        f = f << (i * 8);
+        mask |= f;
       }
-      wdata = wdata & mask;
-      mask = ~mask;
-      long long wdata_z = wdata | (pmem_read((waddr & ~0x7ull), 8) & mask);
-      pmem_write((waddr & ~0x7ull), 8, wdata_z);
     }
+    wdata = wdata & mask;
+    mask = ~mask;
+    long long wdata_z = wdata | (pmem_read((waddr & ~0x7ull), 8) & mask);
+    // printf("wdata_z = %llx\n", wdata_z);
+    pmem_write((waddr & ~0x7ull), 8, wdata_z);
   }
+  if (waddr == CONFIG_SERIAL_MMIO ) {
+    serial_io_input(wdata);
+  }
+
 }
 
 
 extern "C" void pc_print(long long pc) {
-  printf("pc_reg:0x%08llx\n", pc);
+  printf(" store pc 0x%08llx\n", pc);
 }
 
 //pc_print use example
@@ -101,7 +126,6 @@ extern "C" void pc_print(long long pc) {
 // end
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
 // read .bin
 static char *img_file = NULL;
 static int parse_args(int argc, char *argv[]) {
@@ -126,7 +150,7 @@ static int parse_args(int argc, char *argv[]) {
 void sim_exit() {
   tfp->close();
   delete top;
-  delete contextp;
+  // delete contextp;
 }
 
 void exit_now() {
@@ -140,14 +164,14 @@ void sim_init() {
   tfp = new VerilatedVcdC;
 
   top->trace(tfp, 99);
-  tfp->open("wave.vcd");
+  // tfp->open("wave.vcd");
 }
 
 // CMD
 void isa_reg_display() {
 	for(int i=0; i<32; i=i+2){				//64->16,4 but 8 seem looks better
-		printf("%s\t0x%08lx\t", regs[i], cpuu.gpr[i]);
-		printf("%s\t0x%08lx\n", regs[i+1], cpuu.gpr[i+1]);
+		printf("%s\t0x%08lx\t", regs[i], cpu_gpr[i]);
+		printf("%s\t0x%08lx\n", regs[i+1], cpu_gpr[i+1]);
 	}
 	printf("pc\t0x%08lx\n", cpuu.pc);
 
@@ -168,20 +192,17 @@ static int cmd_c()
       top->rst = 0; top->eval();
       
       if (main_time % 10 == 1) {
-        //printf("pc:0x%lx, instr:0x%08lx\n", top->pc, pmem_read(top->pc, 4));
         if(top->clk == 1) {
           if (top->isram_e == 1) {
-            // printf("top->isram_addr: %08lx \n", top->isram_addr);
             top->isram_rdata = pmem_read(top->isram_addr, 4);
           }
-          //printf("pc:0x%lx, bubble:0x%08lx\n", top->difftest_pc, top->bubble);
           if(main_time >= 60) {
             if(pc >= CONFIG_MBASE && pc <= (CONFIG_MBASE + CONFIG_MSIZE) ) {
               for(int i = 0; i < 32; i++) cpuu.gpr[i] = cpu_gpr[i];
               // sp regs are used for addtion
               if(bubble != 1) {
-                printf("     pc 0x%08lx \n", pc);
-                difftest_step(pc, npc);
+                // printf("     pc 0x%08lx \n", pc);
+                // difftest_step(pc, npc);
               }
             }
           }
